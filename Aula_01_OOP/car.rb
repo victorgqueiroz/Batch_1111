cb5455411b76acf160c9d2a0b534a40bab47a8df
victorguilherme <victorg.queiroz@hotmail.com>
2023-02-07 17:05:16 -0300

    Teste

--- Aula_01_OOP/car.rb
+++ Aula_01_OOP/car.rb
@@ -1,36 +1,114 @@
+require_relative "interface"
+
 class Car
-  def initialize(color)
+  attr_reader :color, :brand
+  attr_writer :color, :brand
+
+  def initialize(color, brand)
     @color = color
     @engine_started = false
+    @brand = brand
+  end
+
+  def start
+    # puts "Starting the car..."
+    power_batteries
+    access_fuel
+    crank_engine
+    turn_on_radio
+    @engine_started = true
+  end
+
+  # private #tudo abaixo disso não fica visivel para o usuário.
+
+  def power_batteries
+    puts "Powering baterries"
+  end
+
+  def access_fuel
+    puts "Accessing fuel"
+  end
+
+  def crank_engine
+    puts "Cranking engine - THE MONSTER IS ONNN BABY"
+  end
+
+  def turn_on_radio
+    puts "Playing MOTORHEADDDDD"
+  end
+
+  def engine_started?
+    return @engine_started
+  end
+
+  def stop
+    @engine_started = false
+  end
+
 end
 
-def start
-  puts "Starting the car..."
-  @engine_started = true
-end
 
-def engine_started?
-  return @engine_started
-end
-
-attr_reader :color
-attr_writer :color
-end
-
-red_car = Car.new("red")
-
-puts "Is my car started?"
-puts red_car.engine_started? ? "Yes" : "No"
-
-red_car.start
-
-puts "Is my car started?"
-puts red_car.engine_started? ? "Yes" : "No"
-
-puts "the color of my car is #{red_car.color}"
-
-puts "Insert the new"
+puts "Insert the color and the brand"
 new_color = gets.chomp
-red_car.color = new_color
+new_car = Car.new(new_color, new_brand)
+new_car.color = new_color
+puts "Your new car is color #{new}"
 
-puts "the new car color is #{red_car.color}"
+puts "--" * 20
+
+# puts "Now what's the brand?"
+# new_brand = gets.chomp
+# new_car.brand = new_brand
+# puts "The brand is #{new_car.brand}"
+
+# puts "--" * 20
+
+puts "Let's start the car? Yes or No?"
+start_car = gets.chomp
+
+if start_car == "Yes"
+  new_car.start
+  puts "Oook let's gooo"
+else
+  new_car.stop
+  puts "Whyyy let's turn on this monster baby"
+end
+
+# case start_car
+# when "Yes" = new_car.start
+#   puts "Oook let's gooo"
+# when "No" = new_car.stop
+#   puts "Whyyy let's turn on this monster baby"
+# else
+#   puts "Choose one option"
+# end
+puts "--" * 20
+puts "Is my car started?"
+puts new_car.engine_started? ? "Yes" : "No"
+
+# new_car.start
+
+# puts "Is my car started?"
+# puts new_car.engine_started? ? "Yes" : "No"
+
+# puts "the new car color is #{new_car.color}"
+
+puts "Do you want stop the car? Yes or No"
+stop_car = gets.chomp
+
+if stop_car == "Yes"
+  new_car.stop
+  puts "Let's turn on again"
+else
+  new_car.start
+  puts "Your car is on FIRE!!"
+end
+
+# case stop_car
+# when "Yes"
+#   puts "Let's turn on again"
+# when "No"
+#   puts "Your car is on FIRE!!"
+# else
+#   puts "Choose one option"
+# end
---
+++ Aula_01_OOP/interface.rb
@@ -0,0 +1,40 @@
+require_relative "car.rb"
+
+puts "Insert the color"
+new_color = gets.chomp
+new_car.color = new_color
+puts "the color of my car is #{new_car.color}"
+
+puts "Now what's the brand?"
+new_brand = gets.chomp
+new_car.brand = new_brand
+puts "The brand is #{new_car.brand}"
+
+puts "Let's start the car? Yes or No?"
+start_car = gets.chomp
+new_car.start = start_car
+
+case start_car
+when "Yes"
+  puts "Oook let's gooo"
+when "No"
+  puts "Whyyy let's turn on this monster baby"
+else
+  puts "Choose one option"
+end
+
+puts "Is my car started?"
+puts new_car.engine_started? ? "Yes" : "No"
+
+puts "Do you want stop the car? Yes or No"
+stop_car = gets.chomp
+new_car.stop = stop_car
+
+case stop_car
+when "Yes"
+  puts "Let's turn on again"
+when "No"
+  puts "Your car is on FIRE!!"
+else
+  puts "Choose one option"
+end
